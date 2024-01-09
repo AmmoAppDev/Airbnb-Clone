5000ca0c2f3b27fcb7a2a157bd157023b18ab324
Justin Ammons <Justinammons77@gmail.com>
2024-01-08 22:00:49 -0600

    Initial Commit

---
+++ AirBnB Clone/AirBnB_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBnB_CloneApp.swift
+//  AirBnB Clone
+//
+//  Created by Justin Ammons on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct AirBnB_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
